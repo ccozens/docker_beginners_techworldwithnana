1c94ada3f7e6772f082cf294fd3a3a41788ca2eb
ccozens <ccozens@gmail.com>
2023-04-04 09:58:11 +0100

    updted package author and changed server all let to const, and updated require for import syntgax

--- app/server.js
+++ app/server.js
@@ -1,48 +1,48 @@
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-let MongoClient = require('mongodb').MongoClient;
-let bodyParser = require('body-parser');
-let app = express();
+import express from 'express';
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import { MongoClient } from 'mongodb';
+import { urlencoded, json } from 'body-parser';
+const app = express();
 
-app.use(bodyParser.urlencoded({
+app.use(urlencoded({
   extended: true
 }));
-app.use(bodyParser.json());
+app.use(json());
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(join(__dirname, "index.html"));
   });
 
 app.get('/profile-picture', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+  const img = readFileSync(join(__dirname, "images/profile-1.jpg"));
   res.writeHead(200, {'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
 
 // use when starting application locally
-let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+const mongoUrlLocal = "mongodb://admin:password@localhost:27017";
 
 // use when starting application as docker container
-let mongoUrlDocker = "mongodb://admin:password@mongodb";
+const mongoUrlDocker = "mongodb://admin:password@mongodb";
 
 // pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
-let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+const mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
 // "user-account" in demo with docker. "my-db" in demo with docker-compose
-let databaseName = "my-db";
+const databaseName = "my-db";
 
 app.post('/update-profile', function (req, res) {
-  let userObj = req.body;
+  const userObj = req.body;
 
   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
-    let db = client.db(databaseName);
+    const db = client.db(databaseName);
     userObj['userid'] = 1;
 
-    let myquery = { userid: 1 };
-    let newvalues = { $set: userObj };
+    const myquery = { userid: 1 };
+    const newvalues = { $set: userObj };
 
     db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
       if (err) throw err;
@@ -55,14 +55,14 @@ app.post('/update-profile', function (req, res) {
 });
 
 app.get('/get-profile', function (req, res) {
-  let response = {};
+  const response = {};
   // Connect to the db
   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
-    let db = client.db(databaseName);
+    const db = client.db(databaseName);
 
-    let myquery = { userid: 1 };
+    const myquery = { userid: 1 };
 
     db.collection("users").findOne(myquery, function (err, result) {
       if (err) throw err;
